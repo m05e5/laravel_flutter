a7d107694a70abccd9f0c309dac8247407d3982e
m05e5 <calebmoses1111@gmail.com>
2021-04-20 10:15:16 +0100

    kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,5 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'jwt.verify' => \App\Http\Middleware\JwtMiddleware::class,
-        //'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
     ];
 }
