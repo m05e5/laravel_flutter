3bec4f627322f5e611a42662c349b646629ec6bf
m05e5 <calebmoses1111@gmail.com>
2021-04-26 11:52:02 +0100

    token expired removed

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -26,14 +26,13 @@ class JwtMiddleware extends BaseMiddleware
         } catch (Exception $e) {
             if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
                 return response()->json(['status' => 'Token is Invalid']);
-            }
-            // else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
-            // return response()->json(['status' => 'Token is Expired']);
-            // }
-             else {
+            } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+                //return response()->json(['status' => 'Token is Expired']);
+                return $next($request);
+            } else {
                 return response()->json(['status' => 'Authorization Token not found']);
             }
-        } 
+        }
         return $next($request);
     }
 }
