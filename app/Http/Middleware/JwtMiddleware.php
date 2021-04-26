671f7543c3037d9b753210435252b8601ac7bdbe
m05e5 <calebmoses1111@gmail.com>
2021-04-26 11:49:26 +0100

    nullify omgUrl

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -24,12 +24,13 @@ class JwtMiddleware extends BaseMiddleware
         try {
             $user = FacadesJWTAuth::parseToken()->authenticate();
         } catch (Exception $e) {
-            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
                 return response()->json(['status' => 'Token is Invalid']);
-            }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
-                //return response()->json(['status' => 'Token is Expired']);
-                return $next($request);
-            }else{
+            }
+            // else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+            // return response()->json(['status' => 'Token is Expired']);
+            // }
+             else {
                 return response()->json(['status' => 'Authorization Token not found']);
             }
         }
